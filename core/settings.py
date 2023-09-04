7632af76fb129d7dc7c2dfee34a838c7a3b8f237
emiginx <emiginx@mail.com>
2023-09-04 23:12:01 +0100

    Added pypcog2 for easy postres database connection

--- core/settings.py
+++ core/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "1GQoSEoFe8Wr9Gob6ebg",
+        "HOST": "containers-us-west-195.railway.app",
+        "PORT": "6852",
     }
 }
 
