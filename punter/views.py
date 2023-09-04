e2082b8c9bf31f12e35702c1d43a866db7b8b753
emiginx <emiginx@mail.com>
2023-09-04 05:08:39 +0100

    Added Pagination to home view

--- punter/views.py
+++ punter/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import *
+import datetime 
 
 # Create your views here.
 def index(request):
     categories =  Category.objects.all()
     predictions = Prediction.objects.all()
-    #todays_date = 
-    #todays_time = 
-    context = {'categories': categories, 'predictions': predictions}
+    paginator = Paginator(predictions, 1)  # Show 12 predictions per page.
+    page_number = request.GET.get("page")
+    predictions_obj = paginator.get_page(page_number)
+    todays_datetime = datetime.datetime.now() 
+    context = {'categories': categories, 'predictions': predictions_obj, 'todays_datetime': todays_datetime}
     return render(request, "index.html",context)
